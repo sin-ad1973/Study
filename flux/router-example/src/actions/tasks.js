6212f4586e669c069ff4a6ae08582def19d1a55b
onozato <sin.ad1973@gmail.com>
2018-11-06 22:21:21 +0900

    router

---
+++ flux/router-example/src/actions/tasks.js
@@ -0,0 +1,13 @@
+// *** ActionCreator定義 *******************************
+export const inputTask = (task) => ({
+    type: 'INPUT_TASK',
+    payload: {
+        task,
+    }
+});
+export const addTask = (task) => ({
+    type: 'ADD_TASK',
+    payload: {
+        task,
+    }
+});
