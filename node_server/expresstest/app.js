ccbd9b4efcd37a7abad7af9db501d272b197c35b
sin-ad1973 <sin.ad1973@gmail.com>
2018-12-04 23:33:33 +0900

    express, ejs

---
+++ node_server/expresstest/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var ejs = require('ejs');
+var app = express();
+
+app.engine('ejs', ejs.renderFile);
+
+app.get('/', function(req, res) {
+    res.render('temp.ejs', {
+        contents: '<p>hoge</p>'
+    });
+});
+
+var server = app.listen(1234, function() {
+    console.log('サーバを起動しました')
+});
