9a40d7ae3d4dbd7ef24fb68b65b8d68b3e8b3984
sin-ad1973 <sin.ad1973@gmail.com>
2019-01-14 22:27:04 +0900

    AutoTest Selenium Firefox

--- AutoTest/Selenium/src/SamplScript.js
+++ AutoTest/Selenium/src/SamplScript.js
@@ -2,14 +2,14 @@ var webdriver = require('selenium-webdriver');
 var By = require('selenium-webdriver').By;
 var firefox = require('selenium-webdriver/firefox');
 
-// var driver = new webdriver.Builder().forBrowser('firefox').build();
+var driver = new webdriver.Builder().forBrowser('firefox').build();
 
-let driver = new webdriver.Builder()
-    .withCapabilities(webdriver.Capabilities.firefox())
-    .setFirefoxService(new firefox.ServiceBuilder(__dirname+"/../node_modules/geckodriver/geckodriver"))
-    .build();
+// let driver = new webdriver.Builder()
+//     .withCapabilities(webdriver.Capabilities.firefox())
+//     .setFirefoxService(new firefox.ServiceBuilder(__dirname+"/./driver/geckodriver"))
+//     .build();
 
 driver.get("http://example.selenium.jp/reserveApp");
 driver.findElement(By.id("guestname")).sendKeys("サンプルユーザ");
 driver.findElement(By.id("goto_next")).click();
-driver.quit();
+// driver.quit();
