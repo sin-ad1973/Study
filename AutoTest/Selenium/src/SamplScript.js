059aee001370b59ad5c7db7a991f3681a0ae8371
sin-ad1973 <sin.ad1973@gmail.com>
2019-01-16 22:49:38 +0900

    AutoTest success

--- AutoTest/Selenium/src/SamplScript.js
+++ AutoTest/Selenium/src/SamplScript.js
@@ -1,15 +1,16 @@
 var webdriver = require('selenium-webdriver');
 var By = require('selenium-webdriver').By;
-var firefox = require('selenium-webdriver/firefox');
 
-var driver = new webdriver.Builder().forBrowser('firefox').build();
-
+// ここの記述は[geckodriver]がこのスクリプトと同一階層に置かれていれば必要ない。
+// var firefox = require('selenium-webdriver/firefox');
 // let driver = new webdriver.Builder()
 //     .withCapabilities(webdriver.Capabilities.firefox())
 //     .setFirefoxService(new firefox.ServiceBuilder(__dirname+"/./driver/geckodriver"))
 //     .build();
 
+// warning:[selenium-webdriver]が最新バージョン(4-alpha)だとdriver.quitでエラーになる。
+var driver = new webdriver.Builder().forBrowser('firefox').build();
 driver.get("http://example.selenium.jp/reserveApp");
 driver.findElement(By.id("guestname")).sendKeys("サンプルユーザ");
 driver.findElement(By.id("goto_next")).click();
-// driver.quit();
+driver.quit();
