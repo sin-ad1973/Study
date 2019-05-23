c3d1ddc3a85f3c65b52780913bd3f3409724b0a6
sin-ad1973 <sin.ad1973@gmail.com>
2019-05-23 23:31:05 +0900

    chapter9(Exception2)

---
+++ ruby-book/chapters/chapter9/chapter9_3-3.rb
@@ -0,0 +1,16 @@
+def currency_of(country)
+    case country
+    when :japan
+        'yen'
+    when :us
+        'dollar'
+    when :india
+        'rupee'
+    else
+        # 意図的に例外を発生させる
+        raise ArgumentError.new("無効な国名です。#{country}")
+    end
+end
+
+currency_of(:japan)
+currency_of(:italy)
---
+++ ruby-book/chapters/chapter9/chapter9_6-4.rb
@@ -0,0 +1,24 @@
+# 正常に終了した場合
+ret = 
+begin
+    'OK'
+rescue
+    'error'
+ensure
+    'ensure'
+end
+
+puts ret
+
+# 例外が発生した場合
+ret = 
+begin
+    'OK'
+    1 / 0
+rescue
+    'error'
+ensure
+    'ensure'
+end
+
+puts ret
