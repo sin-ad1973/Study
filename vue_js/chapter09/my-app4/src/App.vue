8a131076249306382d5d2c9eea319309e3485a2a
sin-ad1973 <sin.ad1973@gmail.com>
2019-09-24 22:46:41 +0900

    Vue Router

---
+++ vue_js/chapter09/my-app4/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/" exact>Home</router-link>
+      <router-link to="/product">Product</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.router-link-active {
+    background: #e25193
+}
+
+</style>
