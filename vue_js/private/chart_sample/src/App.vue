509e5a54e2d3680f3e033d125bb2768a957554dd
sin-ad1973 <sin.ad1973@gmail.com>
2020-02-04 01:09:53 +0900

    VUE CHART

---
+++ vue_js/private/chart_sample/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ChartBarSample/>
+  </div>
+</template>
+
+<script>
+import ChartBarSample from './components/ChartBarSample.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ChartBarSample
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
