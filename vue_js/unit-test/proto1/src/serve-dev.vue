7537da87cafa679dd9437c065b536208d6adb55a
sin-ad1973 <sin.ad1973@gmail.com>
2020-02-24 23:08:18 +0900

    VUE TEST / CSS

---
+++ vue_js/unit-test/proto1/src/serve-dev.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import Proto1 from '@/proto1.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Proto1
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <proto1 />
+  </div>
+</template>
