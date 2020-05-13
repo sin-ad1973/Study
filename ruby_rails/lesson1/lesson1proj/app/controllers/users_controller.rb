cc249b2663f0f724826533e75dc1a8f55305560d
sin-ad1973 <sin.ad1973@gmail.com>
2020-05-14 01:09:01 +0900

    Ruby create

--- ruby_rails/lesson1/lesson1proj/app/controllers/users_controller.rb
+++ ruby_rails/lesson1/lesson1proj/app/controllers/users_controller.rb
@@ -3,11 +3,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    if params[:username] == 'ryooopan'
-      @user = User.find_by(:username => 'ryooopan')
-    elsif params[:username] == 'moyahima'
-      @user = User.find_by(:username => 'moyahima')
-    end
+    @user = User.find_by(:username => params[:username])
     # @user = Hash.new
     # if params[:username] == 'ryooopan'
     #   @user[:name] = 'Ryo Suzuki'
--- ruby_rails/lesson1/lesson1proj/db/schema.rb
+++ ruby_rails/lesson1/lesson1proj/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_052731) do
+ActiveRecord::Schema.define(version: 2020_05_13_153526) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
