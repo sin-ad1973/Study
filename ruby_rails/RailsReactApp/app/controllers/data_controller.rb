f016b019d9648fe872298cf8e784beba019358b6
sin-ad1973 <sin.ad1973@gmail.com>
2020-06-13 18:12:18 +0900

    Rails React

---
+++ ruby_rails/RailsReactApp/app/controllers/data_controller.rb
@@ -0,0 +1,19 @@
+class DataController < ApplicationController
+  def index
+  end
+
+  def ajax
+    url = 'https://news.yahoo.co.jp/pickup/rss.xml'
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    render plain:Hash::from_xml(response.body).to_json
+  end
+  # def ajax
+  #   if params[:name] then
+  #     data = Datum.where('name like ?', '%' + params[:name] + '%')
+  #   else
+  #     data = Datum.all
+  #   end
+  #   render plain:data.to_json
+  # end
+end
