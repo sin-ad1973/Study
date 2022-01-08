093399436d124abc93338c3884df13192581427b
sin-ad1973 <sin.ad1973@gmail.com>
2022-01-08 21:08:38 +0900

    vue js 学習

--- vue_js/my-project/src/components/Menu.vue
+++ vue_js/my-project/src/components/Menu.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="hello">
-    <h1 class='msg'>{{ msg2 }}</h1>
-    <button @click="onClick">テスト</button><br>
+  <div class="menu">
+    <!-- <h1 class='msg'>{{ msg2 }}</h1> -->
+    <button @click="onClick">テスト</button>
     <button @click="toPage1">ページ1へ</button>
     <button @click="toPage2">ページ2へ</button>
   </div>
@@ -34,18 +34,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.menu {
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+button {
+  background-color: #fff;
+  border: 1;
+  border-width: 1px;
+  /* border-color: #ccc; */
+  /* border-bottom-color: #ccc; */
+  border-radius: 3px;
+  margin: 1px;
+  padding: 3px;
+  /* min-width: 150px; */
 }
 </style>
--- vue_js/my-project/src/components/Page1.vue
+++ vue_js/my-project/src/components/Page1.vue
@@ -1,3 +1,14 @@
 <template>
-    <div>ページ1</div>
+    <div class="container">ページ1</div>
 </template>
+
+<style scoped>
+.container {
+    position: absolute;
+    left: 200px;
+    width: 100px;
+    height: 100px;
+    background-color: #009;
+    color: #fff;
+}
+</style>
