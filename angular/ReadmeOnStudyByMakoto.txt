11c5aaedaf0088ee89482b419332075c231b3d17
sin-ad1973 <sin.ad1973@gmail.com>
2023-11-24 13:11:35 +0900

    Angular公式チュートリアル完了

--- angular/ReadmeOnStudyByMakoto.txt
+++ angular/ReadmeOnStudyByMakoto.txt
@@ -1,2 +1,5 @@
 # ngコマンドを実行する前に下記を実行し、Powershellのスクリプト実行ポリシーを変更すること
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+
+#json-server起動
+json-server --watch db.json
--- angular/my-app/src/app/app.component.ts
+++ angular/my-app/src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterOutlet, RouterModule, HomeComponent],
   // templateUrl: './app.component.html',
-  template: `<h1>test.{{ title }}</h1>
-  <app-home></app-home>
+  // template: `<h1>test.{{ title }}</h1>
+  // <app-home></app-home>
+  // `,
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/w3c-no-bars.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
   `,
   styleUrl: './app.component.css'
 })
