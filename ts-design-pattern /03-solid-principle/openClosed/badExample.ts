8ba25276c2fb12ea9017208bf086ecdd51d915ef
sin-ad1973 <sin.ad1973@gmail.com>
2023-12-12 21:09:43 +0900

    デザインパターン(TypeScript)

---
+++ ts-design-pattern /03-solid-principle/openClosed/badExample.ts
@@ -0,0 +1,43 @@
+export {}
+
+// 従業員の職位
+type Grade = "junior" | "middle" | "senior" | "expert";
+
+class Employee {
+    constructor(
+        public name: string,
+        public grade: Grade,
+    ) {}
+}
+
+class BonusCalculator {
+    constructor(public base: number) {}
+
+    getBonus(employee: Employee): number {
+        if (employee.grade === "junior") {
+            return Math.floor(this.base * 1.1);
+        } else if (employee.grade === "middle") {
+            return Math.floor(this.base * 1.5);
+        } else if (employee.grade === "senior") {
+            return Math.floor(this.base * 2);
+        } else {
+            return Math.floor(this.base * 3);
+        }
+    }
+}
+
+function run() {
+    const emp1 = new Employee("Yamada", "junior");
+    const emp2 = new Employee("Suzuki", "middle");
+    const emp3 = new Employee("Tanaka", "senior");
+    const emp4 = new Employee("Tanaka", "expert");
+
+    const bonusCalculator = new BonusCalculator(100);
+    
+    console.log(bonusCalculator.getBonus(emp1));
+    console.log(bonusCalculator.getBonus(emp2));
+    console.log(bonusCalculator.getBonus(emp3));
+    console.log(bonusCalculator.getBonus(emp4));
+}
+
+run();
